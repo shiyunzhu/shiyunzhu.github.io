74d72e2c06d8ae93ca172e9f470735f81ba3f92a
Shiyun Zhu <szhu@caltech.edu>
2025-10-26 20:16:44 -0400

    add more conversation questions

--- app/conversationStarterData.ts
+++ app/conversationStarterData.ts
@@ -1,6 +1,25 @@
 const conversationStarterData = [
     "What was your favourite food growing up?", 
     "What did you want to do (as a job) when you were younger?",
+    "What is your favorite move?",
+    "What is the most beautiful place you have seen?",
+    "If you won the lottery, what would you do with the money?",
+    "If you could travel anywhere in the world, where would you go?",
+    "What was your favorite game as a child (or activity you liked to do)?",
+    "What is your favorite animaml at the zoo or aquarium?",
+    "Would you go to Mars if you could?",
+    "Make one bold prediction about the future",
+    "Is AI a \"bubble\" or will it replace all our jobs?",
+    "What is your favorite insect?",
+    "What was better when you were younger?",
+     "If you had a time machine and could travel anywhere in the world at any time, where would you go?",
+     "If you had to live in a deserted island for a year, and you could bring 3 items, what would you bring?",
+     "Do you have any irrational fears?",
+     "Have you ever gotten really lucky (or unlucky)?",
+     "Do you think aliens exist?",
+     "If you could go to any artist's concert (living or dead), who would it be?",
+     "What is your favorite ice cream flavor?"
+
 ]
 
 export default conversationStarterData;
